faf087700549c743909e8b4a3310579f7da3a622
Umair Ahmad <uahmad@techtiz.com>
2024-10-31 12:26:03 +0500

    ticket sp-75 fixes

--- components/kbSection/Question.js
+++ components/kbSection/Question.js
@@ -1,40 +1,51 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Typography, Collapse } from "@mui/material";
 import DescriptionIcon from "@mui/icons-material/Description";
-import Link from "next/link";
 
-function Question({ text, description, title }) {
+function Question({ text, description, }) {
+  const [expandedItems, setExpandedItems] = useState({});
+  const toggleExpand = (itemId) => {
+    setExpandedItems((prevState) => ({
+      ...prevState,
+      [itemId]: !prevState[itemId],
+    }));
+  };
+
   return (
-    <Link
-      href={{
-        pathname: "/Knowledge-base",
-        query: { text, description, title }, // Pass text and description via query parameters
+    <Box
+      onClick={() => toggleExpand(description)}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "950px",
+        margin: "0 auto",
+        padding: 2,
+        cursor: "pointer",
+        borderBottom: "1px solid #ccc",
+        "&:hover": {
+          backgroundColor: "#f5f5f5",
+        },
       }}
-      passHref
     >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          maxWidth: "950px",
-          margin: "0 auto",
-          padding: 2,
-          cursor: "pointer", // Indicate that the question is clickable
-          "&:hover": {
-            backgroundColor: "#f5f5f5", // Change background on hover
-          },
-        }}
-      >
-        <DescriptionIcon
-          sx={{ marginRight: 1, fontSize: 18, color: "#107eec" }}
-        />
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <DescriptionIcon sx={{ marginRight: 1, fontSize: 18, color: "#107eec" }} />
         <Typography variant="body1" sx={{ fontSize: 15, color: "#107eec" }}>
-          {text}{" "}
-          {/* <span style={{ color: "black" }}> (1 minute read)</span> */}
+          {text}
+        </Typography>
+        <Typography sx={{ marginLeft: "auto", fontSize: 24, color: "#107eec" }}>
+          {expandedItems[description] ? "-" : "+"}
         </Typography>
       </Box>
-    </Link>
+
+      <Collapse in={expandedItems[description]} timeout="auto" unmountOnExit>
+        <Typography variant="body2" sx={{ marginTop: 2 }}>
+          {description}
+        </Typography>
+      </Collapse>
+    </Box>
   );
 }
 
 export default Question;
+
+
